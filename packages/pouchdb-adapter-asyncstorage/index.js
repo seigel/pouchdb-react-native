c576933a95bef79a4a0d5088c200f0aeead35aca
stockulus <stockulus@icloud.com>
2016-06-07 14:42:12 +0200

    next steps

--- packages/pouchdb-adapter-asyncstorage/index.js
+++ packages/pouchdb-adapter-asyncstorage/index.js
@@ -1,22 +1,18 @@
 import './polyfill'
 
+import asyncstorageDown from 'asyncstorage-down'
 import CoreLevelPouch from 'pouchdb-adapter-leveldb-core'
 import { extend } from 'js-extend'
 
-const asyncstorageDown = require('asyncstorage-down')
-
-function AsyncStoragePouch(opts, callback) {
-  var _opts = extend({
+function AsyncStoragePouch (opts, callback) {
+  const _opts = extend({
     db: asyncstorageDown
   }, opts)
 
   CoreLevelPouch.call(this, _opts, callback)
 }
 
-AsyncStoragePouch.valid = function () {
-  return true
-};
-AsyncStoragePouch.name = 'asyncstorage'
+AsyncStoragePouch.valid = () => true
 AsyncStoragePouch.use_prefix = false
 
 export default function (PouchDB) {
