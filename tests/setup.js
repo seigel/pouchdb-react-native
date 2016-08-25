7bcc41229518ba2bbd50e6a5214047553b533188
stockulus <stockulus@icloud.com>
2016-08-25 12:58:50 +0200

    get it working

--- tests/setup.js
+++ tests/setup.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const path = require('path')
+/* eslint-disable */
 
-const pouchDir = path.resolve(__dirname, '/../pouchdb-original/packages/node_modules')
+const pouchDir = require('path').resolve(__dirname + '/../pouchdb-original/packages/node_modules')
 
 require('babel-register')({
   presets: ['react-native'],
@@ -18,17 +18,19 @@ const Module = require('module')
 
 const _require = Module.prototype.require
 
-const reqPouch = () => _require(require.resolve('./pouchdb-for-coverage/')).default
-const reqPouchModule = name => _require(require.resolve(`../pouchdb-original/packages/node_modules/${name}`))
+const reqPouch = () => _require.call(null, require.resolve('./pouchdb-for-coverage/')).default
+const reqPouchModule = name => _require.call(null, require.resolve(`../pouchdb-original/packages/node_modules/${name}`))
+const reqAdapter = () => _require.call(null, require.resolve('../packages/pouchdb-adapter-asyncstorage')).default
 
 const map = fs
-  .readdirSync(path.resolve(__dirname, '/../pouchdb-original/packages/node_modules'))
+  .readdirSync(__dirname + '/../pouchdb-original/packages/node_modules')
   .reduce((total, name) => {
     total[name] = reqPouchModule.bind(null, name)
     return total
   }, {
     '../../packages/node_modules/pouchdb-for-coverage': reqPouch,
-    '../../packages/node_modules/pouchdb': reqPouch
+    '../../packages/node_modules/pouchdb': reqPouch,
+    'pouchdb-adapter-asyncstorage': reqAdapter
   })
 
 Module.prototype.require = function patchedRequire (name) {
@@ -37,7 +39,6 @@ Module.prototype.require = function patchedRequire (name) {
   if (callback) {
     return callback()
   }
-
   return _require.call(this, name)
 }
 
