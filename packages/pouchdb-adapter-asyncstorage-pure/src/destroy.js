47d47720449fd55d6e80e34156d1faf6cbf258ab
stockulus <stockulus@icloud.com>
2016-08-25 17:29:55 +0200

    correct interface

--- packages/pouchdb-adapter-asyncstorage-pure/src/destroy.js
+++ packages/pouchdb-adapter-asyncstorage-pure/src/destroy.js
@@ -1,5 +1,5 @@
 'use strict'
 
-export default function (opts, callback) {
+export default function (db, opts, callback) {
   callback()
 }
---
+++ packages/pouchdb-adapter-asyncstorage-pure/src/do_compaction.js
@@ -0,0 +1,5 @@
+'use strict'
+
+export default function (db, id, revs, callback) {
+  callback()
+}
