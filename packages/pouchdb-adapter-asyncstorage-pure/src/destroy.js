627bda0c656b40abc233ffab33ad1821f208813f
stockulus <stockulus@icloud.com>
2016-08-30 18:32:23 +0200

    destroy

--- packages/pouchdb-adapter-asyncstorage-pure/src/destroy.js
+++ packages/pouchdb-adapter-asyncstorage-pure/src/destroy.js
@@ -1,6 +1,12 @@
 'use strict'
 
+import { close as closeDatabase } from './databases'
+
 export default function (db, opts, callback) {
-  console.warn('destroy', db, opts, callback)
-  callback()
+  db.storage.destroy(db.opts.name.slice(7), error => {
+    if (error) callback(error)
+
+    closeDatabase(db.dbOpts.name)
+    callback()
+  }
 }
