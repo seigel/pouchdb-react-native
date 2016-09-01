21dc60072b8f385cd9e89e1da5b70afdc166baa2
stockulus <stockulus@icloud.com>
2016-09-01 09:48:56 +0200

    get testing working for pure adapter

--- tests/setup.js
+++ tests/setup.js
@@ -21,6 +21,7 @@ const _require = Module.prototype.require
 const reqPouch = () => _require.call(null, require.resolve('./pouchdb-for-coverage/')).default
 const reqPouchModule = name => _require.call(null, require.resolve(`../pouchdb-original/packages/node_modules/${name}`))
 const reqAdapter = () => _require.call(null, require.resolve('../packages/pouchdb-adapter-asyncstorage')).default
+const reqAdapterPure = () => _require.call(null, require.resolve('../packages/pouchdb-adapter-asyncstorage-pure')).default
 const reqLevelAdapter = () => _require.call(null, require.resolve('../packages/pouchdb-adapter-leveldb-core-rn')).default
 
 const map = fs
@@ -32,6 +33,7 @@ const map = fs
     '../../packages/node_modules/pouchdb-for-coverage': reqPouch,
     '../../packages/node_modules/pouchdb': reqPouch,
     'pouchdb-adapter-asyncstorage': reqAdapter,
+    'pouchdb-adapter-asyncstorage-pure': reqAdapterPure,
     'pouchdb-adapter-leveldb-core-rn': reqLevelAdapter
   })
 
