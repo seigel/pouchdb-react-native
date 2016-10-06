70363904d59d593f9403962d863bd45d23412cd0
ewnd9 <ewndnine@gmail.com>
2016-10-07 05:42:05 +0800

    Add node@4 to travis, add 'use strict'

--- packages/pouchdb-react-native/setup.js
+++ packages/pouchdb-react-native/setup.js
@@ -1,23 +1,26 @@
+'use strict'
+
 const fs = require('fs')
+const path = require('path')
+const globby = require('globby')
 
-let PATH = './node_modules/pouchdb-binary-utils/package.json'
-let packageContent
-try {
-  packageContent = JSON.parse(fs.readFileSync(PATH))
-} catch (e) {
-  PATH = '../pouchdb-binary-utils/package.json'
-  packageContent = JSON.parse(fs.readFileSync(PATH))
+const root = path.resolve(__dirname, '..')
+const PATHS = globby.sync('**/pouchdb-binary-utils/package.json', { cwd: root })
+
+for (const p of PATHS) {
+  const PATH = path.join(root, p)
+  const packageContent = JSON.parse(fs.readFileSync(PATH))
+
+  packageContent['react-native'] = {
+    './lib/index.js': './lib/index.js',
+    './src/base64.js': './src/base64.js',
+    './src/base64StringToBlobOrBuffer.js': './src/base64StringToBlobOrBuffer.js',
+    './src/blob.js': './src/blob.js',
+    './src/binaryStringToBlobOrBuffer.js': './src/binaryStringToBlobOrBuffer.js',
+    './src/blobOrBufferToBase64.js': './src/blobOrBufferToBase64.js',
+    './src/blobOrBufferToBinaryString.js': './src/blobOrBufferToBinaryString.js',
+    './src/typedBuffer.js': './src/typedBuffer.js'
+  }
+
+  fs.writeFileSync(PATH, JSON.stringify(packageContent, null, '  ') + '\n', 'utf8')
 }
-
-packageContent['react-native'] = {
-  './lib/index.js': './lib/index.js',
-  './src/base64.js': './src/base64.js',
-  './src/base64StringToBlobOrBuffer.js': './src/base64StringToBlobOrBuffer.js',
-  './src/blob.js': './src/blob.js',
-  './src/binaryStringToBlobOrBuffer.js': './src/binaryStringToBlobOrBuffer.js',
-  './src/blobOrBufferToBase64.js': './src/blobOrBufferToBase64.js',
-  './src/blobOrBufferToBinaryString.js': './src/blobOrBufferToBinaryString.js',
-  './src/typedBuffer.js': './src/typedBuffer.js'
-}
-
-fs.writeFileSync(PATH, JSON.stringify(packageContent, null, '  ') + '\n', 'utf8')
